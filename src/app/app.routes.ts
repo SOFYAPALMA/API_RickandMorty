a0a08aa06c8ce87f0fa1400486d47e87ba20e35f
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-04-01 09:30:35 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,13 @@ import { LocationComponent } from './Pages/location/location.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/characters', pathMatch: 'full' }, //al abrir la aplicacion lleva a pagina principal characters
-  { path: '', component: CharactersComponent }, //pagina no existente lleva a pagina principal characters
-  { path: 'characters', component: CharactersComponent },  //pagina characters
-  { path: 'details', component: DetailsComponent }, //pagina details
-  { path: 'location', component: LocationComponent }, //pagina location
+  { path: '', component: CharactersComponent }, //direccion no existe lleva a pagina principal characters
+  { path: 'characters', component: CharactersComponent },  //si la url dice characters, muestre el componente CharactersComponent
+  { path: 'details', component: DetailsComponent }, //si la url dice details, muestre el componente DetailsComponent
+  { path: 'location', component: LocationComponent }, //si la url dice location, muestre el componente LocationComponent
 
   {
     path: 'characters/:id',
-    component: CharactersComponent,
+    component: CharactersComponent, //se pasa el parametro id en characters para que muestre la info de characters
   },
 ];

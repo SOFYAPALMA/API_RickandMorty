40849cf4044601faffae7ada0f8432ab52929890
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-03-31 22:31:46 -0500

    creacion de peticiones a webApi para lista y consullta

--- src/app/Services/RickAndMortyService.ts
+++ src/app/Services/RickAndMortyService.ts
@@ -1,13 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { appsettings } from '../Settings/appsettings';
+import { results } from '../Models/results';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickAndMortyService {
   private http = inject(HttpClient);
-  private apiUrl:string = appsettings.apiUrl + "info";
-
+  private apiUrl:string = appsettings.apiUrl + "results";
+  
   constructor() { }
+
+  lista(){
+    return this.http.get<results[]>(this.apiUrl);
+  }
+
+  obtener(){
+    return this.http.get<results>('${this.apiUrl}/${id}');
+  }
+
 }

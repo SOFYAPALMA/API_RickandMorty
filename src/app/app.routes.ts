e308777d7cdf74b4aac740d32bb6abd1ab8a0658
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-04-01 09:22:43 -0500

    configuracion de direccion en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { CharactersComponent } from './Pages/characters/characters.component';
+import { DetailsComponent } from './Pages/details/details.component';
+import { LocationComponent } from './Pages/location/location.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/characters', pathMatch: 'full' }, //al abrir la aplicacion lleva a pagina principal characters
+  { path: '', component: CharactersComponent }, //pagina no existente lleva a pagina principal characters
+  { path: 'characters', component: CharactersComponent },  //pagina characters
+  { path: 'details', component: DetailsComponent }, //pagina details
+  { path: 'location', component: LocationComponent }, //pagina location
+
+  {
+    path: 'characters/:id',
+    component: CharactersComponent,
+  },
+];
